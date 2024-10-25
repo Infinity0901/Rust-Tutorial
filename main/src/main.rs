15a824dfffcb3aebf3bb4b5c83c405cb375278f3
Infinity0901 <147404427+Infinity0901@users.noreply.github.com>
2024-10-25 14:39:55 +0200

    main

--- main/src/main.rs
+++ main/src/main.rs
@@ -6,3 +6,4 @@ fn main() {
 // Declaration avec la date, le poids, la valeur de l'objet, matériaux
 // Etre vague dans la description : ecrire seulement stickers
 // Trouver des templates de déclaration de douane
+// Pas de lettre recommandé 
